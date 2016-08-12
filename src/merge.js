d869d953a34fab213564f6bbd82a584d41ae0d91
szabototo89 <szabototo89@gmail.com>
2016-08-12 23:08:37 +0200

    Finish merge and test it

--- src/merge.js
+++ src/merge.js
@@ -1,8 +1,13 @@
 const merge = (...actualDependencies) => (create) => {
-  return (...dependencies) => create(actualDependencies.map((actualDependency, index) => ({
-    ...actualDependency,
-    ...dependencies[index]
-  })));
+  return (...dependencies) => {
+    const depsSize = Math.max(actualDependencies.length, dependencies.length);
+    const deps = Array(depsSize).fill({});
+
+    return create(...deps.map((_, index) => ({
+      ...(actualDependencies[index] || {}),
+      ...(dependencies[index] || {})
+    })));
+  }
 };
 
 export default merge;
--- test/merge.test.js
+++ test/merge.test.js
@@ -1,2 +1,74 @@
 import merge from '../src/merge';
 import expect from 'expect';
+
+describe('merge() function', () => {
+  const create = (...dependencies) => dependencies;
+
+  it('should merge actual dependencies into dependencies when there is no naming conflict between them', () => {
+    // arrange
+    const actualDependencies = {
+      a: 1,
+      b: 2
+    };
+
+    const dependencies = {
+      c: 3,
+      d: 4
+    };
+
+    const underTest = merge(actualDependencies);
+
+    // act
+    const result = underTest(create)(dependencies);
+
+    // assert
+    expect(result).toEqual([{
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4
+    }]);
+  });
+
+  it('should merge multiple actual dependencies when there is more actual', () => {
+    // arrange
+    const actualDependencies = [
+      { a: 1, b: 2 },
+      { c: 3, d: 4 }
+    ];
+
+    const dependencies = { c: 3, d: 4 };
+
+    const underTest = merge(...actualDependencies);
+
+    // act
+    const result = underTest(create)(dependencies);
+
+    // assert
+    expect(result).toEqual([
+      { a: 1, b: 2, c: 3, d: 4 },
+      { c: 3, d: 4 }
+    ]);
+  });
+
+  it('should merge multiple actual dependencies when there is less actual', () => {
+    // arrange
+    const actualDependencies = { c: 3, d: 4 };
+
+    const dependencies = [
+      { a: 1, b: 2 },
+      { c: 3, d: 4 }
+    ];
+
+    const underTest = merge(actualDependencies);
+
+    // act
+    const result = underTest(create)(...dependencies);
+
+    // assert
+    expect(result).toEqual([
+      { a: 1, b: 2, c: 3, d: 4 },
+      { c: 3, d: 4 }
+    ]);
+  });
+});
